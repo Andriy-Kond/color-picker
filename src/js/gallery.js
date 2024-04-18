9461ae23d1f5154902b1193a70a033a5d48449f0
Andrii <andrii.kondratovich@gmail.com>
2024-04-18 18:06:58 +0300

    ADD: gallery markup

---
+++ src/js/gallery.js
@@ -0,0 +1,25 @@
+import countriesArr from "../dataBases/countries.json";
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+};
+
+const galleryMarkup = countriesArr
+  .map(({ flag, name: countryName, capital, currency, language, population, infected }) => {
+    return `<li class="gallery__item">
+        <div class="gallery__thumb">
+          <img src="${flag}" alt="Флаг" width="320" />
+        </div>
+        <h2>${countryName}</h2>
+        <p><b>Столица: </b> ${capital}</p>
+        <p><b>Валюта:</b>${currency}</p>
+        <p><b>Официальный язык:</b>${language}</p>
+        <p><b>Население:</b> ${population} человек</p>
+        <p style="color: ${infected ? "red" : "green"};"> ${infected ? "Заражені" : "Здорові"}</p>
+      </li>
+    
+      `;
+  })
+  .join("");
+
+refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
